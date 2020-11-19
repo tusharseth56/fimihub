b98a6768cb5850252d313b18e24e3731359e7bd3
tusharseth56 <42093057+tusharseth56@users.noreply.github.com>
2020-11-19 20:03:47 +0530

    order event

--- database/migrations/2020_11_12_111739_create_orders_table.php
+++ database/migrations/2020_11_12_111739_create_orders_table.php
@@ -83,9 +83,41 @@ class CreateOrdersTable extends Migration
             $table->decimal('total_amount', 8, 2)->nullable();
             $table->decimal('delivery_fee', 8, 2)->nullable();
             $table->decimal('tax', 8, 2)->nullable();
-            $table->string('order_status')->comment('1-pending,2-placed,3-packed,4-picked,5-recieved')->nullable();
-            $table->string('payment_status')->comment('1-pending,2-success,3-failed')->nullable();
+            $table->tinyInteger('order_status')->comment('1-failed,
+                                                    2-user_cancel,
+                                                    3-pending,
+                                                    4-resto_cancel,
+                                                    5-placed,
+                                                    6-packed,
+                                                    7-picked,
+                                                    8-rider_cancel,
+                                                    9-received,
+                                                    10-refunded')->nullable();
+            $table->tinyInteger('payment_status')->comment('1-pending,2-success,3-failed')->nullable();
             $table->tinyInteger('payment_type')->comment('1-stripe,2-paypal,3-COD');
+            $table->tinyInteger('visibility')->default('0');
+            $table->timestamp('deleted_at', 0)->nullable();
+            $table->timestamps();
+        });
+
+
+        Schema::create('order_events', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf16';
+            $table->collation = 'utf16_general_ci';
+            $table->increments('id');
+            // foreign key of orders table
+            $table->integer('order_id')->unsigned();
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade')->onUpdate('cascade');
+            // foreign key of users table
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            
+            $table->tinyInteger('order_status')->comment('based on user type')->nullable();
+            $table->string('order_commment')->nullable();
+            $table->tinyInteger('order_feedback')->nullable();
+            $table->tinyInteger('feedback_comment')->nullable();
+            $table->tinyInteger('user_type')->comment('1-Rider,2-Restaurent')->nullable();
             $table->tinyInteger('visibility')->default('0');
             $table->timestamp('deleted_at', 0)->nullable();
             $table->timestamps();
@@ -99,6 +131,7 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('order_events');
         Schema::dropIfExists('orders');
         Schema::dropIfExists('cart_submenus');
         Schema::dropIfExists('carts');
