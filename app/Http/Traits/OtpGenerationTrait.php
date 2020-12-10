0d73ab793d8193530733fabcade40857a2da67c5
tusharseth56 <42093057+tusharseth56@users.noreply.github.com>
2020-12-10 12:55:22 +0530

    up4

--- app/Http/Traits/OtpGenerationTrait.php
+++ app/Http/Traits/OtpGenerationTrait.php
@@ -67,7 +67,7 @@ trait OtpGenerationTrait {
             $client = new Client($account_sid, $auth_token);
             $client->messages->create(
                 // Where to send a text message (your cell phone?)
-                "8840212040",
+                "+918840212040",
                 array(
                     'from' => $twilio_number,
                     'body' => $messageMsg
